12abcf301a88363a4b5750058d26bff5bb7fb258
WITmrdoodlepants4 <97747045+WITmrdoodlepants4@users.noreply.github.com>
2024-10-02 15:55:19 -0400

    Create main.py
    - Main function created

---
+++ main.py
@@ -0,0 +1,39 @@
+from ui import FunVirusApp
+import sys
+import pychar
+#[OTHER IMPORTS]
+
+
+#Aidan, Peter, Modesto, & Ilya
+#Software Engineering
+#Professor Kenechukwu
+
+def main():
+#Run UI
+    init_db()
+    app = QApplication(sys.argv)
+    ex = FunVirusApp()
+    # Where the fun happens
+    '''
+    - Change Audio Output/Volume
+    - Add Fun Image/Gifs
+    - Change display coloring
+    '''
+    ex.show()
+    sys.exit(app.exec_())
+
+
+#run VIRUS-----------------------------------------------------------------!
+'''
+- take in credentials from user
+- send credentials to VirusCreator.py
+- 
+'''
+
+###
+
+
+if __name__ == '__main__':
+    main()
+    print("PETER")
+
