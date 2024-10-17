c40cea6351f36deb4c277bff06655da8650e9437
WITmrdoodlepants4 <97747045+WITmrdoodlepants4@users.noreply.github.com>
2024-10-17 15:38:39 -0400

    audio + rotate.py
    - Added audio and rotate functions as a stand alone file in the FUNctionalities folder

---
+++ FUNctionalities/audio.py
@@ -0,0 +1,29 @@
+import random
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.api.audioclient import ISimpleAudioVolume
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+# -65.0 is 0, 0 is 100
+# volume.SetMasterVolumeLevel(-38.0, None)
+
+# adjust current volume by + 4
+# current = volume.GetMasterVolumeLevel()
+# volume.SetMasterVolumeLevel(current + 6.0, None)
+
+# find all apps that are running
+sessions = AudioUtilities.GetAllSessions()
+
+# loop through apps
+for session in sessions:
+    volume = session._ctl.QueryInterface(ISimpleAudioVolume)
+
+    # set volume of all apps running
+    if session.Process:
+        # set volume of app randomly between 0 and 1 inclusive (0 = 0, 1 = 100)
+        random_volume = random.random()
+        volume.SetMasterVolume(random_volume, None)
