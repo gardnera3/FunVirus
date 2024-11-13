dbb5aac34a52561402c9a85d0f41b3279e2f2ec7
gardnera3 <71507240+gardnera3@users.noreply.github.com>
2024-11-13 13:53:25 -0500

    Added timers to fullscreen popups (5mins)
    Added 5 min timers to BSOD and update so they wait 300 seconds before starting.

--- Virus.py
+++ Virus.py
@@ -1,9 +1,10 @@
 import importlib
 import threading
+import time
 # import main  # Uncomment this if `main` is needed
 
 # Sample toggle array and file array
-toggle_array = [0, 1, 0, 1, 1, 1, 1, 0]
+toggle_array = [0, 1, 0, 0, 0, 0, 0, 0]
 file_array = [
     "FUNctionalities.stewietest", 
     "FUNctionalities.BSOD", 
@@ -17,6 +18,10 @@ file_array = [
 
 # Function to import a module based on its name
 def import_module(name):
+    if name == "FUNctionalities.BSOD":
+        time.sleep(300)  # Wait for 5 minutes
+    if name == "FUNctionalities.UpdateScreen":
+        time.sleep(300)  # Wait for 5 minutes
     importlib.import_module(name)
 
 # List to keep track of threads
@@ -26,6 +31,7 @@ threads = []
 for i in range(len(toggle_array)):
     if toggle_array[i] == 1:
         name = file_array[i]
+
         # Create and start a new thread for each module that needs to be imported
         thread = threading.Thread(target=import_module, args=(name,))
         threads.append(thread)
